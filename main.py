14563f7c43bcb09d6b82813fccc1d9e1e85a3087
FlickZaur <zaurmarem87@mail.ru>
2022-05-07 08:27:07 +0300

    конец

--- main.py
+++ main.py
@@ -19,7 +19,14 @@ def project():
     <li><a href="#цели">цели</a></li>
     <li><a href="#cursi">Курс по HTML</a></li>
     <li><a href="#pit">Самый низ сайта</a></li>
-</ul></nav>
+</ul>
+<ol>
+<li><a href="#sp1">Упорядоченные и неупоряноченный списки</a></li>
+<li><a href="#sp2">Списки описаний</a></li>
+<li><a href="#br">Преформатированный текст и переносы</a></li>
+<li><a href="#href">Виды ссылок</a></li>
+<li><a href="img">Картинки</a></li>
+</nav>
 <h4><a name="цели"></a>Цели проекта</h4>
 <ol>
     <li><a name="cursi"></a>показать разнообразные фишки в
@@ -37,7 +44,7 @@ def project():
         пример:
     <br>&lt;footer&gt;
     <br>&lt;/footer&gt;
-    <p>Есть 2 вида списков:</p>
+    <p><a name="sp1"></a>Есть 2 вида списков:</p>
     <ul>
         <li>Не упорядоченный</li>
         <li>&lt;ul&gt;</li>
@@ -49,7 +56,7 @@ def project():
         <li>разделитель элементов тоже &lt;li&gt;</li>
         <li>дополнительные расширения найдёте в <a href="#docx">вордовском файле</a></li>
     </ol>
-    <h2>Список описаний</h2>
+    <h2><a name="sp2"></a>Список описаний</h2>
     <dl>
 <dt>&lt;dl&gt;</dt>
         <dd>огранечитель списка</dd>
@@ -58,7 +65,7 @@ def project():
         <dt>&lt;dd&gt;</dt>
         <dd>подписи под фразой</dd>
     </dl>
-    <p>есть выделение цитаты с помощью <q>&lt;q&gt;</q></p>
+    <p>есть выделение цитаты с помощью <q>&lt;q&gt;</q></p><a name="br"></a>
     <h3>Преформатированный текст</h3>
     <p>выполняется с помощью &lt;pre&gt;</p>
     <pre>Пример
@@ -75,7 +82,7 @@ def project():
 </article>
 <article>
     <div>
-        <p>ожно подключить ссылку с помощью &lt;a&gt;</p>
+        <p>можно подключить ссылку с помощью &lt;a&gt;</p><a name="href"></a>
         <p>пример: <q>&lt;a href="ссылка"&gt;<cite>фраза</cite>&lt;/a&gt;</q></p>
         <p><a href="https://colorscheme.ru/html-colors.html">Цвета для css файла в web</a></p>
         <p>можно также <a href="static/teamplates/dop.html">переключиться</a> на другой файл html</p>
@@ -88,12 +95,9 @@ def project():
     </div>
 </article>
 <article>
-    <div>Посленее, что мы будем проходить - <a href="static/teamplates/img.html"><ins></ins>ИЗОБРАЖЕНИЯ</a></div>
-
+    <div><a name="img"></a>Посленее, что мы будем проходить - <a href="static/teamplates/img.html">
+    <ins>ИЗОБРАЖЕНИЯ</ins></a></div>
 </article>
-
-
-
 <footer>
     <h3><a name="docx" href="HTML_and_CSS.docx" download>более подробная информация об HTML(конспект)</a></h3>
     <h6><a name="pit"></a>Если ты долистал до этого момента, значит тебе было интересно, спасибо)</h6>
